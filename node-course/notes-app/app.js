f731607fbff86f7663c8ad49169f71d9c4d41a74
Dakota Erickson <derickson29@gmail.com>
2021-01-07 10:59:24 -0600

    files/functionality for adding/removing notes

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,7 +1,64 @@
+const fs = require('fs');
 const chalk = require('chalk');
-const getNotes = require('./notes.js');
+const yargs = require('yargs');
+const notes = require('./notes.js');
 
-const msg = getNotes()
-console.log(msg);
+// customize yargs version
+yargs.version('1.1.0');
 
-console.log(chalk.blue.bold.inverse('Error!'));
+// create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note.',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body);
+    }
+});
+
+// create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note.',
+    builder: {
+        title: {
+            describe: 'Title of note to remove.',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.removeNote(argv.title);
+    }
+});
+
+// create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes.',
+    handler: function () {
+        console.log('List all notes!');
+    }
+});
+
+// create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note.',
+    handler: function () {
+        console.log('Read a note!');
+    }
+});
+
+yargs.parse();
--- node-course/notes-app/notes.js
+++ node-course/notes-app/notes.js
@@ -1,5 +1,68 @@
+const chalk = require('chalk');
+const fs = require('fs');
+
+
 const getNotes = function () {
     return 'Your notes...';
 }
 
-module.exports = getNotes;
+const addNote = function (title, body) { // load the existing notes from notes.json
+    const notes = loadNotes();
+
+    // check existing notes to ensure we aren't creating a duplicate title
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title;
+    })
+    if (duplicateNotes.length === 0) { // add the new note
+        notes.push({title: title, body: body});
+
+        // save the notes
+        saveNotes(notes);
+        console.log(chalk.green('New note added.'));
+    } else {
+        console.log(chalk.red('Note title already taken.'));
+    }
+
+
+}
+
+const saveNotes = function (notes) { // convert the javscript object to JSON and write the data
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
+}
+
+const loadNotes = function () { 
+    // try catch block to ensure that if notes.json doesn't exist we can create it
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (e) {
+        return []
+    }
+}
+
+const removeNote = function (title) { // load the existing notes from the file
+    const notes = loadNotes();
+
+    // filter out any notes with the matching title
+    const notesToKeep = notes.filter(function (note) {
+        return note.title !== title;
+    })
+    if (notesToKeep.length < notes.length) {
+
+        console.log(chalk.green('Note removed!'));
+
+        // save the new list of notes without the matching title
+        saveNotes(notesToKeep);
+    } else {
+        console.log(chalk.red('No note found!'));
+    }
+
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote
+};

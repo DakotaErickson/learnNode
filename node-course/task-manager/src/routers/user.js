045951780702a6ddb52dc84eaeaa55ca69146442
Dakota Erickson <derickson29@gmail.com>
2021-01-15 11:50:26 -0600

    fully implemented authentication and added endpoints for user profile pictures

--- node-course/task-manager/src/routers/user.js
+++ node-course/task-manager/src/routers/user.js
@@ -2,6 +2,22 @@ const express = require('express');
 const User = require('../models/user.js');
 const router = new express.Router();
 const auth = require('../middleware/auth.js');
+const multer = require('multer');
+const sharp = require('sharp');
+
+// set the destination for uploads
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image.'));
+        }
+
+        cb(undefined, true);
+    }
+});
 
 // create new
 router.post('/users', async (req, res) => {
@@ -88,5 +104,40 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 })
 
+
+// upload profile picture
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+    req.user.avatar = buffer;
+
+    //req.user.avatar = req.file.buffer;
+    await req.user.save();
+    res.send();
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+})
+
+// delete profile picture
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send();
+})
+
+// fetch profile picture
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user || !user.avatar) {
+            throw new Error();
+        }
+        res.set('Content-Type', 'image/png');
+        res.send(user.avatar);
+
+    } catch (e) {
+        res.status(404).send();
+    }
+})
 
 module.exports = router;
